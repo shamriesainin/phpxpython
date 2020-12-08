a33d03b136a8f9e9b7b74668d199e3a371e57683
shamriesainin <73682722+shamriesainin@users.noreply.github.com>
2020-12-08 09:19:33 +0800

    Add files via upload

---
+++ test.php
@@ -0,0 +1,6 @@
+<?php
+$x = 1;
+$output = exec("C:\Users\ITU21\AppData\Local\Programs\Python\Python38\python.exe test.py $x"); 
+var_dump($output);
+echo $output; 
+?>
---
+++ test.py
@@ -0,0 +1,7 @@
+#C:\Users\ITU21\AppData\Local\Programs\Python\Python38\python.exe
+import sys
+var = sys.argv
+
+print("Hello from Python, values(s) received is: " + str(var))
+#print("Hello1")
+#print("Hello2")
